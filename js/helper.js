f7ca74ee93c39ea326fe613d7c8184a27c65264a
Christopher Avila <asternux@Christophers-MacBook-Pro.local>
2016-02-23 06:52:45 -0600

    Preparation to before delivering P2
     Changes to be committed:
    	new file:   images/mypic.jpg
    	new file:   images/pro1.jpg
    	new file:   images/pro2.jpg
    	new file:   images/pro3.jpg
    	modified:   index.html
    	modified:   js/helper.js
    	modified:   js/resumeBuilder.js

--- js/helper.js
+++ js/helper.js
@@ -13,7 +13,8 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+//var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span class="white-text">%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -60,12 +61,13 @@ var googleMap = '<div id="map"></div>';
 
 
 /*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
+The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run.
+Don't delete! It hooks up your code to the button you'll be appending.
 */
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName() || function(){};
-    $('#name').html(iName);  
+    $('#name').html(iName);
   });
 });
 
@@ -86,6 +88,10 @@ function logClicks(x,y) {
 
 $(document).click(function(loc) {
   // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+
+  logClicks(x,y);
 });
 
 
@@ -109,9 +115,9 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
+  /*
   For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  appended to #mapDiv in resumeBuilder.js.
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
@@ -130,19 +136,29 @@ function initializeMap() {
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
-      locations.push(school.location);
-    });
+
+for (var school in education.schools){
+  locations.push(education.schools[school].location);
+}
+   // education.schools.forEach(function(school){
+   //   locations.push(school.location);
+   // });
 
     // iterates through work locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
-      locations.push(job.location);
-    });
+
+for (var job in work.jobs) {
+  locations.push(work.jobs[job].location);
+}
+    //work.jobs.forEach(function(job){
+    //  locations.push(job.location);
+    //});
+
+
 
     return locations;
   }
@@ -176,7 +192,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -238,7 +254,7 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,328 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+
+//--------------------------------------
+// Ejercicio_1
+//--------------------------------------
+// $("#main").append("Chris Avila");
+//--------------------------------------
+
+
+//--------------------------------------
+// Ejercicio_2
+//--------------------------------------
+// var awesomeThoughts = "I am ChrisAvila and I am AWESOME!";
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// $("#main").append(funThoughts);
+// console.log(awesomeThoughts);
+// console.log(funThoughts);
+//--------------------------------------
+
+
+//--------------------------------------
+// Ejercicio_3
+//--------------------------------------
+// var name = "Chris Avila";
+// var formattedName = HTMLheaderName.replace("%data%", name);
+// var role = "Web Developer";
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+//
+// var skills = ["smart", "programming", "html", "css", "js"];
+// $("#main").append(skills);
+// $("#main").append(skills[0]);
+// $("#main").append(skills.lenght);
+//--------------------------------------
+
+
+//--------------------------------------
+// Ejercicio_4
+//--------------------------------------
+//var bio = {
+//	"name": "Chris Avila",
+//	"role": "Solutions Architect",
+//
+//	"contacts": {
+//		"mobile": "52-1-55-61049160",
+//		"email": "avila.christopher@gmail.com",
+//		"github": "christophavila",
+//		"location": "Mexico",
+//		},
+//
+//	"welcomeMessage": "Bienvenidos a mi sitio",
+//	"skills": [
+//		"smart", "programming", "html", "css", "js"
+//		],
+//	"bioPic": "images/fry.jpg"
+//};
+//
+// $("#main").append(bio.name);
+// bio.city =  "CDMX";
+// bio.twitter = "@christophavila";
+// $("#main").append(bio.city);
+// $("#main").append(bio["city"]);
+//--------------------------------------
+
+//--------------------------------------
+// Ejercicio_5
+//--------------------------------------
+// var work = {};
+// work.position = "FE-ND Student";
+// work.employer = "Udacity";
+// work.years = 0.5;
+//
+// var education = {};
+// education["name"] = "UNAM";
+// education["years"] = "1997-2001";
+// education["city"] = "Mexico";
+//
+// $("#main").append(work["position"]);
+// $("#main").append(education.name);
+//--------------------------------------
+
+
+//--------------------------------------
+// Ejercicio_6
+//--------------------------------------
+//var education = {
+//	"schools": [{
+//		"name": "UNAM",
+//		"city": "Mexico",
+//		"degree": "BA",
+//		"major": ["CompEng"],
+//		"dates": 2001
+//	}, {
+//		"name": "ITAM",
+//		"city": "Mexico",
+//		"degree": "Master",
+//		"major": ["ComSci"],
+//		"dates": 2015
+//	}],
+//	"onlineCourses": [{
+//		"name": "JavaScript",
+//		"school": "Udacity",
+//		"degree": "FE-ND",
+//		"major": ["WebDeveloper"],
+//		"dates": 2016
+//	}]
+//}
+// $("#main").append(work["position"]);
+// $("#main").append(education.name);
+//--------------------------------------
+
+
+
+//--------------------------------------
+// OBJECTS
+//--------------------------------------
+var bio = {
+	"name": "Christopher Avila",
+	"role": "ICT Consultant",
+	"welcomeMessage": "Welcome to my Site that has been built in Udacity FE-ND",
+	"contacts": {
+		"mobile": "52-1-55-61049160",
+		"email": "avila.christopher@gmail.com",
+		"github": "christophavila",
+		"twitter": "@christophavila",
+		"location": "Mexico"
+	},
+	"skills": [
+		"programming", "html/css/js", "IMS", "UnifiedComms"
+	],
+	"bioPic": "images/mypic.jpg"
+};
+
+var education = {
+	"schools": [{
+		"name": "UNAM",
+		"location": "Mexico",
+		"degree": "BA",
+		"major": ["CompEng"],
+		"dates": 2001,
+		"url": "https://unam.mx"
+	}, {
+		"name": "ITAM",
+		"location": "Mexico",
+		"degree": "Master",
+		"major": ["ComSci"],
+		"dates": 2015,
+		"url": "https://itam.mx"
+	}],
+	"onlineCourses": [{
+		"name": "JavaScript",
+		"location": "San Francisco",
+		"degree": "FE-ND",
+		"major": ["WebDeveloper"],
+		"dates": 2016,
+		"url": "https://udacity.com"
+	}]
+};
+
+var work = {
+	"jobs": [{
+		"employer": "Ericsson",
+		"title": "CSI Senior",
+		"location": "San Jose",
+		"dates": "2001-2005",
+		"description": "Install/Integ/Config: IMS Solutions (EMM2.1, IMT3.0), 3PP. Oracle 10g, BEA WLSS, Intervoice and TexttoSpeech"
+	}, {
+		"employer": "Ericsson",
+		"title": "SA Experienced",
+		"location": "Stockholm",
+		"dates": "2005-2012",
+		"description": "Develop Ericsson IMS Solution Architectures. \(Nodes involved are ICS5.0, MTAS2.0, VPNBT1.0\)"
+	}, {
+		"employer": "BroadSoft",
+		"title": "Senior Sales Engineer",
+		"location": "Washington DC",
+		"dates": "20012-2015",
+		"description": "Supporting pre-sales technical activities forBroadSoft’s flagship Unified Communications Platforms and Mobile Applications (BroadWorks, BroadCloud, BroadTouch)"
+	 	}]
+};
+
+var projects = {
+	"projects": [{
+		"title": "IN Network",
+		"dates": "2001-2005",
+		"description": "Create IP-BB for IN",
+		"images": ["images/pro1.jpg"]
+	}, {
+		"title": "IMS Mexico",
+		"dates": "2006-2012",
+		"description": "CDeploy first IMS Network",
+		"images": ["images/pro2.jpg"]
+	}, {
+		"title": "CUAD",
+		"dates": "2012-2015",
+		"description": "UC Communications",
+		"images": ["images/pro3.jpg"]
+	}]
+};
+
+
+//--------------------------------------
+// BIO
+//--------------------------------------
+ var formattedName = HTMLheaderName.replace("%data%", bio.name);
+ var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+ var contactMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+ var contactEmail = HTMLemail.replace("%data%", bio.contacts.email);
+ var contactTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+ var contactGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+ var contactLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+
+ var bioPicture = HTMLbioPic.replace('%data%', bio.bioPic);
+ var bioMessage = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+
+ $("#header").append(contactMobile);
+ $("#header").append(contactEmail);
+ $("#header").append(contactTwitter);
+ $("#header").append(contactGithub);
+ $("#header").append(contactLocation);
+
+ $('#header').append(bioPicture);
+ $('#header').append(bioMessage);
+
+
+//--------------------------------------
+// BIO.SKILLS
+//--------------------------------------
+if (bio.skills.lenght !== 0) {
+
+ $("#header").append(HTMLskillsStart);
+
+ var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+ $("#skills").append(formattedSkill);
+ formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+ $("#skills").append(formattedSkill);
+ formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+ $("#skills").append(formattedSkill);
+ formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+ $("#skills").append(formattedSkill);
+
+}
+
+
+//--------------------------------------
+// JOBS
+//--------------------------------------
+
+function displayWork() {
+ for (job in work.jobs) {
+	$("#workExperience").append(HTMLworkStart);
+
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+	$(".work-entry:last").append(formattedEmployerTitle);
+
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    $(".work-entry:last").append(formattedDates);
+
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    $(".work-entry:last").append(formattedDescription);
+ }
+}
+
+displayWork ();
+
+//--------------------------------------
+// PROJECTS
+//--------------------------------------
+
+projects.display = function() {
+	for (project in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedProTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		$(".project-entry:last").append(formattedProTitle);
+
+		var formattedProDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		$(".project-entry:last").append(formattedProDates);
+
+		var formattedProDesc = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedProDesc);
+
+		if (projects.projects[project].images.lenght !== 0) {
+			for (image in projects.projects[project].images) {
+				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+				$(".project-entry:last").append(formattedImage);
+			}
+		}
+
+	}
+}
+projects.display ();
+
+//--------------------------------------
+// InternationalizationButtonFunction
+//--------------------------------------
+function inName(name) {
+
+// Needs to be added because of error:
+// "Uncaught TypeError: Cannot read property ‘trim’ of undefined"
+// https://discussions.udacity.com/t/uncaught-typeerror-cannot-read-property-trim-of-undefined/41446/5
+  var name = $('#name').html();
+
+	name = name.trim().split(" ");
+	console.log(name);
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+	return name[0] +" "+name[1];
+
+}
+
+$("#main").append(internationalizeButton);
+
+//--------------------------------------
+// How to see the Map
+//--------------------------------------
+$("#mapDiv").append(googleMap);
+
